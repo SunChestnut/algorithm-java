ac2fa3b11e1a5e01dc379eeba57f44fd4baf4137
syl <18222521019@163.com>
2019-06-28 19:09:47 +0800

    优化第六题

--- src/main/java/com/sun/SolutionSix.java
+++ src/main/java/com/sun/SolutionSix.java
@@ -1,10 +1,62 @@
 package com.sun;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 题目：将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列
+ * ----------------------------------------------------------
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+ * L   C   I   R
+ * E T O E S I I G
+ * E   D   H   N
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+ * 请你实现这个将字符串进行指定行数变换的函数：string convert(string s, int numRows);
+ * ----------------------------------------------------------
+ * 示例 1:
+ * 输入: s = "LEETCODEISHIRING", numRows = 3
+ * 输出: "LCIRETOESIIGEDHN"
+ * ----------------------------------------------------------
+ * 示例 2:
+ * 输入: s = "LEETCODEISHIRING", numRows = 4
+ * 输出: "LDREOEIIECIHNTSG"
+ * 解释:
+ * L     D     R
+ * E   O E   I I
+ * E C   I H   N
+ * T     S     G
+ * ----------------------------------------------------------
  *
  * @author: syl
  * @date: 2019/6/27 15:54
  **/
 public class SolutionSix {
+
+    private static String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        StringBuilder ret = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                ret.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) {
+                    ret.append(s.charAt(j + cycleLen - i));
+                }
+            }
+        }
+        return ret.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "LEETCODEISHIRING";
+        int numRows = 3;
+        String resultStr = "LCIRETOESIIGEDHN";
+        System.out.println(resultStr.equals(convert(str, numRows)));
+    }
+
+
 }
