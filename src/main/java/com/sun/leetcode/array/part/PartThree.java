804593effabe396c524957a664ac5a427eb30cbe
syl <18222521019@163.com>
2019-08-02 17:42:52 +0800

    update PartThree and DailyTest

---
+++ src/main/java/com/sun/leetcode/array/part/PartThree.java
@@ -0,0 +1,33 @@
+package com.sun.leetcode.array.part;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO
+ *
+ * @author: syl
+ * @date: 2019/7/29 9:41
+ **/
+public class PartThree {
+
+    private static int maxProfit(int[] prices) {
+        if (prices.length < 2) {
+            return 0;
+        }
+        // 利润
+        int profit = 0;
+        // 当前最小值
+        int curMin = prices[0];
+
+        for (int i = 0; i < prices.length; i++) {
+            profit = Math.max(profit, prices[i] - curMin);
+            curMin = Math.min(curMin, prices[i]);
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/test/java/com/sun/daily/DailyTest.java
@@ -0,0 +1,50 @@
+package com.sun.daily;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO
+ *
+ * @author: syl
+ * @date: 2019/7/30 11:17
+ **/
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+public class DailyTest {
+
+    @Test
+    public void testA() {
+        List<String> list = new ArrayList<>();
+        list.add(null);
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add(null);
+
+        System.out.println(list.size());
+
+        System.out.println(list.get(4));
+    }
+
+    /**
+     * 斐波拉契数列
+     */
+    @Test
+    public void fibTest() {
+        int n1 = 0, n2 = 1, n3, count = 10;
+        System.out.println(n2);
+
+        for (int i = 2; i < count; i++) {
+            n3 = n1 + n2;
+            System.out.println(n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
