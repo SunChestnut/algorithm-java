ee192c16746a5fb2881785b87d00daa0367ae5e2
syl <18222521019@163.com>
2020-01-18 21:46:22 +0800

    添加注释

--- src/main/java/com/sun/algorithm/str/ReverseString.java
+++ src/main/java/com/sun/algorithm/str/ReverseString.java
@@ -22,14 +22,22 @@ import java.util.Arrays;
  */
 public class ReverseString {
 
+    /**
+     * 双指针法：left为左指针，right为右指针，开始时左指针指向首元素，右指针指向尾元素，
+     * 交换两个指针所指向的元素，并向中间移动，直到两个指针相遇，遍历s.length/2次
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(1)
+     *
+     * @param s 源数组
+     */
     public static void reverseString(char[] s) {
-        int j = s.length - 1;
-        for (int i = 0; i <= s.length / 2 - 1; i++) {
-            if (j >= s.length / 2) {
-                if (s[i] != s[j]) {
-                    swap(s, i, j);
+        int right = s.length - 1;
+        for (int left = 0; left <= s.length / 2 - 1; left++) {
+            if (right >= s.length / 2) {
+                if (s[left] != s[right]) {
+                    swap(s, left, right);
                 }
-                j--;
+                right--;
             }
         }
 
