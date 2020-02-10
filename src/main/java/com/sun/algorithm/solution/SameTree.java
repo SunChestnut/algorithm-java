98936b2ee4c5bc29256b88bd8b5b24b6e7d3884b
syl <18222521019@163.com>
2020-02-10 09:48:51 +0800

    [ADD] SameTree.java

---
+++ src/main/java/com/sun/algorithm/solution/SameTree.java
@@ -0,0 +1,80 @@
+package com.sun.algorithm.solution;
+
+
+/**
+ * 【简单 Solution100】
+ * 题目：相同的树
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ * ----------------------------------------------------------
+ * 示例 1:
+ * 输入:       1         1
+ *           / \       / \
+ *          2   3     2   3
+ *
+ *         [1,2,3],   [1,2,3]
+ *
+ * 输出: true
+ * ----------------------------------------------------------
+ * 示例 2:
+ * 输入:      1          1
+ *           /           \
+ *          2             2
+ *
+ *         [1,2],     [1,null,2]
+ *
+ * 输出: false
+ * ----------------------------------------------------------
+ * 示例 3:
+ * 输入:       1         1
+ *           / \       / \
+ *          2   1     1   2
+ *
+ *         [1,2,1],   [1,1,2]
+ *
+ * 输出: false
+ *----------------------------------------------------------
+ *
+ * @author syl
+ * @date 2020/02/10 - 09:23
+ */
+public class SameTree {
+
+    public static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        node1.left = new TreeNode(2);
+        node1.right = new TreeNode(3);
+        System.out.println(isSameTree(node1, node1));
+
+        TreeNode node2 = new TreeNode(1);
+        node1.right = new TreeNode(3);
+        System.out.println(isSameTree(node1, node2));
+    }
+}
