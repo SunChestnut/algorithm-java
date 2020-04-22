9b922115b8326466eecb8216453eb692dffe09eb
syl <18222521019@163.com>
2020-04-22 10:32:36 +0800

    [UPDATE] Add Problems

---
+++ src/main/java/com/sun/solution/ExcelSheetColumnTitle.java
@@ -0,0 +1,29 @@
+package com.sun.solution;
+
+/**
+ * 168. Excel表列名称
+ *
+ * @author syl
+ * @date 2020/04/22 - 09:51
+ */
+public class ExcelSheetColumnTitle {
+
+    public String convertToTitle(int n) {
+
+        StringBuilder res = new StringBuilder();
+
+        while (n > 0) {
+            n -= 1;
+            System.out.println(n + " % 26 = " + n % 26);
+            res.insert(0, (char) ('A' + (n % 26)));
+            n /= 26;
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        ExcelSheetColumnTitle solution = new ExcelSheetColumnTitle();
+        System.out.println(solution.convertToTitle(701));
+    }
+
+}
