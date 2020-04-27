12d896a6e70c90597a45faa94a022253cee6d6ee
syl <18222521019@163.com>
2020-04-27 16:22:39 +0800

    [UPDATE] Add Problems

---
+++ src/main/java/com/sun/interview/IsUniqueLCCI.java
@@ -0,0 +1,46 @@
+package com.sun.interview;
+
+/**
+ * 面试题 01.01. 判定字符是否唯一
+ *
+ * @author syl
+ * @date 2020/04/27 - 15:59
+ */
+public class IsUniqueLCCI {
+
+    public boolean isUnique(String astr) {
+        char[] charArray = astr.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            for (int j = i + 1; j < charArray.length; j++) {
+                if (charArray[i] == charArray[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isUniqueT(String astr) {
+
+        int[] arr = new int[26];
+
+        for (char c : astr.toCharArray()) {
+            int index = c - 'a';
+            if (arr[index] == 0) {
+                arr[index] = 1;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        IsUniqueLCCI solution = new IsUniqueLCCI();
+        System.out.println(solution.isUnique("leetcode"));
+        System.out.println(solution.isUnique("abc"));
+
+        System.out.println(solution.isUniqueT("leetcode"));
+    }
+
+}
---
+++ src/main/java/com/sun/solution/SymmetricTree.java
@@ -0,0 +1,90 @@
+package com.sun.solution;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * TODO
+ *
+ * @author syl
+ * @date 2020/04/24 - 10:24
+ */
+public class SymmetricTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+
+    public boolean isSymmetricWithLoop(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+
+            if (t1.val != t2.val) {
+                return false;
+            }
+
+            q.add(t1.left);
+            q.add(t2.right);
+
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(3);
+
+        TreeNode root2 = null;
+
+        SymmetricTree solution = new SymmetricTree();
+        System.out.println(solution.isSymmetric(root));
+        System.out.println(solution.isSymmetric(root2));
+
+        System.out.println(solution.isSymmetricWithLoop(root));
+        System.out.println(solution.isSymmetricWithLoop(root2));
+    }
+
+}
