81570e72eb9009d449f89b7cd3dcad4caa3cfc8f
syl <18222521019@163.com>
2020-07-04 16:03:53 +0800

    [ADD] 1⃣️ Leetcode 658.Find K closest elements

---
+++ src/test/java/com/sun/solution/FindKClosestElementsTest.java
@@ -0,0 +1,26 @@
+package com.sun.solution;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+@Slf4j
+class FindKClosestElementsTest {
+
+
+    private static int[] arr = new int[]{1, 2, 3, 4, 5};
+
+    @Test
+    void findClosestElements() {
+
+        FindKClosestElements solution = new FindKClosestElements();
+        List<Integer> resList = solution.findClosestElements(arr, 4, 3);
+
+        log.info("resList = {}", resList);
+
+        Assert.assertArrayEquals(new Object[]{1, 2, 3, 4}, resList.toArray());
+
+    }
+}
